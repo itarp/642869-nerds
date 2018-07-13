c1961ab13f607ac1179eb2c539cbc35afd03f0a3
Pavel A Taranov <itarpgm@gmail.com>
2018-07-13 13:30:49 +0300

    Почти заврешена верстка

---
+++ js/scirpt.js
@@ -0,0 +1,57 @@
+var link = document.querySelector(".contacts-button");
+    
+var popup = document.querySelector(".modal-write-us");
+var close = popup.querySelector(".modal-close");
+
+var form = popup.querySelector("form");
+var fullname = popup.querySelector("[name=fullname]");
+var email = popup.querySelector("[name=email]");
+
+var isStorageSupport = true;
+var storage = "";
+
+try {
+  storage = localStorage.getItem("fullname");
+} catch (err) {
+  isStorageSupport = false;
+}
+
+link.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.add("modal-show");
+  if (storage) {
+    fullname.value = storage;
+    email.focus();
+  } else {
+    fullname.focus();
+  }
+});
+
+close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.remove("modal-show");
+  popup.classList.remove("modal-error");
+});
+
+form.addEventListener("submit", function (evt) {
+  if (!fullname.value || !email.value) {
+    evt.preventDefault();
+    popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error");
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("fullname", fullname.value);
+    }
+  }
+});
+
+window.addEventListener("keydown", function (evt) {
+if (evt.keyCode === 27) {
+  evt.preventDefault();
+  if (popup.classList.contains("modal-show")) {
+    popup.classList.remove("modal-show");
+    popup.classList.remove("modal-error");
+  }
+}
+});
